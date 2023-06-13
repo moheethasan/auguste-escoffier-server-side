4abb4d8c36d8333856de38fdc96ca89bf326053a
majesty-01 <moheethasan4@gmail.com>
2023-06-13 17:02:20 +0600

    added post api for users

--- index.js
+++ index.js
@@ -27,6 +27,19 @@ async function run() {
 
     const usersCollection = client.db("escoffierDb").collection("users");
 
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
+      }
+
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
